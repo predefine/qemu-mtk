836171a954d6f83cc7c2748a459cf1922fb99f53
predefine <predefine@yandex.ru>
2026-02-07 10:30:49 +0700

    hw: arm: mt6580: add efusec and sej
    Signed-off-by: predefine <predefine@yandex.ru>

--- hw/arm/mt6580.c
+++ hw/arm/mt6580.c
@@ -9,8 +9,10 @@
 #include "hw/char/serial-mtk.h"
 #include "hw/misc/unimp.h"
 
-#define MT6580_GPT_BASE   0x10008000
-#define MT6580_UART0_BASE 0x11005000
+#define MT6580_GPT_BASE    0x10008000
+#define MT6580_EFUSEC_BASE 0x10009000
+#define MT6580_SEJ_BASE    0x1000a000
+#define MT6580_UART0_BASE  0x11005000
 
 static void mt6580_realize(DeviceState *socdev, Error **errp)
 {
@@ -34,13 +36,23 @@ static void mt6580_realize(DeviceState *socdev, Error **errp)
     // TODO: add interrupt controller
     // sysbus_connect_irq
 
+    sysbus_realize(SYS_BUS_DEVICE(&s->efusec), &error_abort);
+    sysbus_mmio_map(SYS_BUS_DEVICE(&s->efusec), 0, MT6580_EFUSEC_BASE);
+
+    sysbus_realize(SYS_BUS_DEVICE(&s->sej), &error_abort);
+    sysbus_mmio_map(SYS_BUS_DEVICE(&s->sej), 0, MT6580_SEJ_BASE);
+
     create_unimplemented_device("topckgen",     0x10000000, 0x1000);
+    create_unimplemented_device("infracfg",     0x10001000, 0x1000);
     create_unimplemented_device("gpio",         0x10005000, 0x1000);
     create_unimplemented_device("wdt",          0x10007000, 0x1000);
     create_unimplemented_device("pmic_wrap",    0x1000f000, 0x1000);
     create_unimplemented_device("iocfg_b",      0x10015000, 0x1000);
     create_unimplemented_device("iocfg_r",      0x10017000, 0x1000);
     create_unimplemented_device("apmixed",      0x10018000, 0x1000);
+    create_unimplemented_device("dbgsys",       0x1011a000, 0x1000);
+    create_unimplemented_device("mcucfg",       0x10200000, 0x1000);
+    create_unimplemented_device("sramrom",      0x10209000, 0x1000);
     create_unimplemented_device("display_pwm",  0x1100f000, 0x1000);
     create_unimplemented_device("mmc0",         0x11120000, 0x1000);
     create_unimplemented_device("mmc1",         0x11130000, 0x1000);
@@ -52,6 +64,10 @@ static void mt6580_init(Object *obj)
     MT6580State *s = MT6580_SOC(obj);
 
     object_initialize_child(obj, "gpt", &s->gpt, TYPE_MTK_GPT);
+
+    object_initialize_child(obj, "efusec", &s->efusec, TYPE_MT6580_EFUSEC);
+
+    object_initialize_child(obj, "sej", &s->sej, TYPE_MT6580_SEJ);
 }
 
 static void mt6580_class_init(ObjectClass *klass, const void *data)
--- include/hw/arm/mt6580.h
+++ include/hw/arm/mt6580.h
@@ -4,27 +4,17 @@
 #include "hw/core/sysbus.h"
 #include "hw/arm/boot.h"
 #include "hw/timer/mtk_gpt.h"
+#include "hw/misc/mt6580_efusec.h"
+#include "hw/misc/mt6580_sej.h"
 
 struct MT6580State {
     SysBusDevice parent_obj;
+
     ARMCPU *cpu[4];
+
     MtkGptState gpt;
-//     qemu_irq irq_table[EXYNOS4210_MAX_INT_COMBINER_IN_IRQ];
-//
-//     MemoryRegion chipid_mem;
-//     MemoryRegion iram_mem;
-//     MemoryRegion irom_mem;
-//     MemoryRegion irom_alias_mem;
-//     MemoryRegion boot_secondary;
-//     MemoryRegion bootreg_mem;
-//     I2CBus *i2c_if[EXYNOS4210_I2C_NUMBER];
-//     OrIRQState pl330_irq_orgate[EXYNOS4210_NUM_DMA];
-//     OrIRQState cpu_irq_orgate[EXYNOS4210_NCPUS];
-//     A9MPPrivState a9mpcore;
-//     Exynos4210GicState ext_gic;
-//     Exynos4210CombinerState int_combiner;
-//     Exynos4210CombinerState ext_combiner;
-//     SplitIRQ splitter[EXYNOS4210_NUM_SPLITTERS];
+    Mt6580EfusecState efusec;
+    Mt6580SejState sej;
 };
 
 #define TYPE_MT6580_SOC "mt6580"
